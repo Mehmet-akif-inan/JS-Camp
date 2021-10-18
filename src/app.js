86b7af220c19cdfaf859bd9d040090a15181bce9
Mehmet Akif Inan <72706177+Mehmet-akif-inan@users.noreply.github.com>
2021-10-19 00:33:27 +0300

    October 18, end of course.

---
+++ src/app.js
@@ -0,0 +1,21 @@
+console.log("Hello World")
+
+let dolarDun = 8.20
+
+let dolarBugun =9.30
+
+console.log(dolarDun)
+
+const euroDun = 11.1
+//euroDun = 11
+
+console.log(euroDun)
+let konutKredileri = ["Konut Kredisi", "Emlak Kredisi","Kamu Konut Kredisi","EmlakÇı Kredisi"]
+console.log("<ul>")
+for(let i=0; i<konutKredileri.length; i++){
+    console.log("<li>"+ konutKredileri[i]+"/li")
+}
+console.log("/ul")
+
+
+console.log(konutKredileri)
